ae87be93e1acaf86622cf88449db42f7f9ff6d6e
李小龙 <xiaolong.li@yechtech.com>
2025-05-20 16:25:09 +0800

    chore: init

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+export default defineConfig({
+  server: {
+    port: 3000,
+    open: true,
+  },
+  plugins: [vue(), vueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
