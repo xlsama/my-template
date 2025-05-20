e35914c1ece4921d7bf8fded99b72689a1ad0124
李小龙 <xiaolong.li@yechtech.com>
2025-05-20 17:20:06 +0800

    chore: update

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
+import { PiniaColada } from '@pinia/colada'
 import { createPinia } from 'pinia'
-
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import { routes } from 'vue-router/auto-routes'
 import App from './App.vue'
-import './assets/main.css'
+
+import './app.css'
 
 const app = createApp(App)
 
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+app.use(router)
 app.use(createPinia())
+app.use(PiniaColada, {})
 
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
+import VueRouter from 'unplugin-vue-router/vite'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
@@ -9,10 +13,35 @@ export default defineConfig({
     port: 3000,
     open: true,
   },
-  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  plugins: [
+    vue(),
+    vueDevTools(),
+    tailwindcss(),
+    VueRouter({
+      dts: 'src/typed-router.d.ts',
+    }),
+    AutoImport({
+      imports: [
+        'vue',
+        'pinia',
+        '@vueuse/core',
+        VueRouterAutoImports,
+      ],
+      dts: 'src/auto-imports.d.ts',
+      dirs: [
+        'src/composables',
+        'src/stores',
+        'src/utils',
+      ],
+      vueTemplate: true,
+    }),
+    Components({
+      dts: 'src/components.d.ts',
+    }),
+  ],
 })
